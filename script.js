e75d7aa02bd15f067b85ae59b6c3c23eb6c67472
mutaician <109066669+mutaician@users.noreply.github.com>
2024-02-01 14:30:07 +0300

    change numbers variables to array

--- script.js
+++ script.js
@@ -16,26 +16,32 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, op) {
-    if (op == '+') {
-        return add(num1, num2)
+    if (Boolean(num1) && Boolean(num2)) {
+        num1 = parseFloat(num1)
+        num2 = parseFloat(num2)
+        if (op == '+') {
+            return add(num1, num2)
+        }
+        else if (op == '-') {
+            return subtract(num1, num2)
+        }
+        else if (op == '*') {
+            return multiply(num1, num2)
+        }
+        else if (op == '/') {
+            return divide(num1, num2)
+        }
+    } else if (op == '-'){
+        return -num1
     }
-    else if (op == '-') {
-        return subtract(num1, num2)
-    }
-    else if (op == '*') {
-        return multiply(num1, num2)
-    }
-    else if (op == '/') {
-        return divide(num1, num2)
-    }
-    else return ""
+    else return 'Error'
 }
 
-function clearDisplay(){
+function clearDisplay() {
     results.textContent = ''
-    firstNumber = ''
+    firstNumber = []
     operator = ''
-    secondNumber = ''
+    secondNumber = []
     numBeforeOperator = true
     nextoperator = ''
 }
@@ -43,49 +49,53 @@ function clearDisplay(){
 function checkTextContent(text) {
     if (clearAfterEquals) {
         clearDisplay()
-        clearAfterEquals = false}
-
+        clearAfterEquals = false
+    }
 
     if (operators.includes(text)) {
-        if (Boolean(operator) && Boolean(secondNumber)) {
+        if (Boolean(operator) && secondNumber.length != 0) {
             nextoperator = text
-            checkTextContent('solvefirst')}
+            checkTextContent('solvefirst')
+        }
         else {
-        operator = text
-        results.textContent += text
-        numBeforeOperator = false
-        } 
+            operator = text
+            results.textContent += text
+            numBeforeOperator = false
+        }
     }
 
     if (!isNaN(text)) {
         results.textContent += text
-        if (!Boolean(firstNumber) || numBeforeOperator) {
-            firstNumber = Number(firstNumber + text)
+        if (firstNumber.length == 0 || numBeforeOperator) {
+            firstNumber.push(text)
         }
-        else if (!Boolean(secondNumber) || !numBeforeOperator) {
-            secondNumber = Number(secondNumber + text)
-            // console.log(secondNumber)
+        else if (secondNumber.length == 0 || !numBeforeOperator) {
+            secondNumber.push(text)
         }
     }
 
-    if (text == '='){
-        let total = operate(firstNumber,secondNumber,operator)
+    if (text == '=') {
+        let total = operate(firstNumber.join(''), secondNumber.join(''), operator)
         results.textContent = total
-        firstNumber = ''
-        secondNumber = ''
+        firstNumber = []
+        secondNumber = []
         clearAfterEquals = true
     }
 
-    if (text == 'solvefirst'){
-        let total = operate(firstNumber,secondNumber,operator)
-        firstNumber = total
-        secondNumber = ''
+    if (text == 'solvefirst') {
+        let total = operate(firstNumber.join(''), secondNumber.join(''), operator)
+        firstNumber = String(total).split('')
+        if (firstNumber[0] == '-') {
+            firstNumber.shift();
+            firstNumber[0] = '-' + firstNumber[0]
+        }
+        secondNumber = []
         operator = nextoperator
         results.textContent = total + operator
     }
 
+    if (text == 'clear') { clearDisplay() }
 
-    if (text == 'clear'){clearDisplay() }
 }
 
 const buttons = document.querySelectorAll('button')
@@ -98,10 +108,10 @@ for (let i = 0; i < buttons.length; i++) {
 let results = document.querySelector('.display')
 
 const operators = ['+', '-', '*', '/']
-let firstNumber = ''
+let firstNumber = []
 let operator = ''
 let nextoperator = ''
-let secondNumber = ''
+let secondNumber = []
 let clearAfterEquals = false
 let numBeforeOperator = true
 
